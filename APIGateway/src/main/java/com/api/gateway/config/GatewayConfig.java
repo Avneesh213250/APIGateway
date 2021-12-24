b11f575fc61861c9c1be884c8d62d4a16cb56e33
Avneesh <Avneesh@DESKTOP-F12T56R>
2021-12-24 17:38:25 +0530

    initial commit

---
+++ APIGateway/src/main/java/com/api/gateway/config/GatewayConfig.java
@@ -0,0 +1,15 @@
+package com.api.gateway.config;
+
+//@Configuration
+public class GatewayConfig {
+
+//	@Bean
+//	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//		return builder.routes()
+//				.route("ABCBank", p -> p.path("/ABCBank/**").uri("http://localhost:8082/"))
+//				.route("StudentData", p -> p.path("/StudentData/**").uri("http://localhost:8084/"))
+//				.route(p -> p.path("/get")
+//						.filters(f -> f.addRequestHeader("Hello", "World")).uri("http://httpbin.org:80"))
+//				.build();
+//	}
+}
